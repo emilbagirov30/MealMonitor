eacc6618cf595a944d4f284b8752d3f12a6adbf9
emil <milbgr@gmail.com>
2025-03-18 02:52:35 +0300

    Initial commit

---
+++ src/main/java/com/emil/mealmonitor/MealmonitorApplication.java
@@ -0,0 +1,13 @@
+package com.emil.mealmonitor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MealmonitorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MealmonitorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/emil/mealmonitor/MealmonitorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.emil.mealmonitor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MealmonitorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
