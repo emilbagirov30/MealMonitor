3e4d040c2b77c7e3ce9e03dabd96ec2eb0178f82
emil <milbgr@gmail.com>
2025-03-18 03:59:32 +0300

    added ExceptionHandler

---
+++ src/main/java/com/emil/mealmonitor/exception/GlobalExceptionHandler.java
@@ -0,0 +1,20 @@
+package com.emil.mealmonitor.exception;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import java.util.Map;
+import java.util.stream.Collectors;
+
+@ControllerAdvice
+public class GlobalExceptionHandlerExceptionHandlerExceptionHandler {
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<Map<String, String>> handleValidationExceptions(MethodArgumentNotValidException ex) {
+        Map<String, String> errors = ex.getBindingResult().getFieldErrors()
+                .stream().collect(Collectors.toMap(FieldError::getField, FieldError::getDefaultMessage));
+        return ResponseEntity.badRequest().body(errors);
+    }
+}
